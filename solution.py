2b75118cb132694c3d3a162c5fdb0051710a202c
Alexander Nizov <nizoff.as@yandex.ru>
2023-04-05 11:28:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,8 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    sum = 0
+    mean = x.mean()
+    for i in range(len(x)):
+        sum += (x[i]-mean)**2
+    return 1/(n-1)*sum # Ваш ответ
