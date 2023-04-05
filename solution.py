506e738413639eed4dc055c59825ad7fe337cfee
Alexander Nizov <nizoff.as@yandex.ru>
2023-04-05 12:35:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,9 @@ import numpy as np
 chat_id = 457863109 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Идея: вместо выборочной дисперсии использовать несмещённую (исправленную) дисперсию
     sum = 0
     mean = x.mean()
     n = len(x)
     for i in range(n):
         sum += (x[i]-mean)**2
-    return 1/(n-1)*sum # Ваш ответ
+    return (1/n)*sum # Ваш ответ
