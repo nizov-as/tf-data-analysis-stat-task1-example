b6f0006f5bec16936e3c5c79de1bd4b72115ca3d
Alexander Nizov <nizoff.as@yandex.ru>
2023-04-06 22:15:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,4 @@ import numpy as np
 chat_id = 457863109 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    sum = 0
-    mean = x.mean()
-    n = len(x)
-    for i in range(n):
-        sum += (x[i]-mean)**2
-    return (1/n)*sum # Ваш ответ
+    return x.mean()
